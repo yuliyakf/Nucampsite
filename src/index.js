025070ef1235d1ca49eb993504e29881e5a2f959
Yuliya Feland <yuliyakf@gmail.com>
2022-04-05 20:41:51 -0400

    react components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+
 
 ReactDOM.render(
   <React.StrictMode>
