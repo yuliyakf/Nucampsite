780e6bc5f2ce4b4edf92cc4832912dc14ec3ebd1
Yuliya Feland <yuliyakf@gmail.com>
2022-04-16 08:56:31 -0400

    extra components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'font-awesome/css/font-awesome.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 
